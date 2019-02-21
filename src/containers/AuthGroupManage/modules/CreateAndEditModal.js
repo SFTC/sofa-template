63d35cb8354aa9a21f45277cdfb19d46b85c6d62
ylq <458909861@qq.com>
2019-02-21 18:49:35 +0800

    权限组修改

--- src/containers/AuthGroupManage/modules/CreateAndEditModal.js
+++ src/containers/AuthGroupManage/modules/CreateAndEditModal.js
@@ -6,6 +6,7 @@ import {
   Form,
   Input,
   Select,
+  Transfer,
 } from 'antd';
 
 import { createStructuredSelector } from 'reselect';
@@ -14,12 +15,11 @@ import { CREATE, EDIT } from 'utils/constants';
 import { injectIntl, intlShape } from 'react-intl';
 import commonMessages from 'utils/commonMessages';
 
-import OperationAuthSelect from './OperationAuthSelect';
 import messages from '../messages';
 
 import { NAMESPACE } from '../constants';
-import { updateEntityModal, postCreateEntity, postEditEntity } from '../actions';
-import { selectEntityModal, selectEntityModalType } from '../selectors';
+import { updateEntityModal, postCreateEntity, postEditEntity, getPrivilegeList } from '../actions';
+import { selectEntityModal, selectEntityModalType, selectOperationAuth } from '../selectors';
 
 const withConnect = connectFactory(NAMESPACE);
 
@@ -34,12 +34,14 @@ function isModify(type) {
   createStructuredSelector({ // 实用reselect性能有明显的提升；
     entityModal: selectEntityModal,
     type: selectEntityModalType,
+    operationAuth: selectOperationAuth,
   }),
   { // 其实这里可以处理掉，当前每引入一个action,需要更新props绑定，更新PropsType，
     // 实际可以直接将action全量引入，但是出于对性能及规范开发的要求，这里仍然使用单独引入的方式；
     updateEntityModal,
     postCreateEntity,
     postEditEntity,
+    getPrivilegeList,
   },
 )
 @Form.create({
@@ -52,12 +54,18 @@ function isModify(type) {
 class CreateAndEditModal extends React.PureComponent {
   static propTypes = {
     entityModal: PropTypes.object.isRequired,
+    operationAuth: PropTypes.array.isRequired,
     updateEntityModal: PropTypes.func.isRequired,
     postCreateEntity: PropTypes.func.isRequired,
     postEditEntity: PropTypes.func.isRequired,
+    getPrivilegeList: PropTypes.func.isRequired,
     intl: intlShape.isRequired,
     type: PropTypes.string.isRequired,
   };
+
+  componentDidMount() {
+    this.props.getPrivilegeList();
+  }
 
   state = {
   };
@@ -67,7 +75,6 @@ class CreateAndEditModal extends React.PureComponent {
     const { type } = this.props.entityModal;
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        values.privileges = this.props.entityModal.data.privileges;
         if (type === CREATE) {
           this.props.postCreateEntity(values);
         } else if (type === EDIT) {
@@ -86,7 +93,7 @@ class CreateAndEditModal extends React.PureComponent {
   }
 
   render() {
-    const { entityModal, intl, type, form } = this.props;
+    const { entityModal, intl, type, operationAuth } = this.props;
     const { data } = entityModal;
 
     const { getFieldDecorator } = this.props.form;
@@ -146,7 +153,25 @@ class CreateAndEditModal extends React.PureComponent {
                 <Input />,
               )}
             </FormItem>
-            <OperationAuthSelect form={form} />
+            <FormItem
+              {...formItemLayout}
+              label={intl.formatMessage(messages.authGroupManage.operationAuth)}
+            >
+              {
+                getFieldDecorator('privileges', {
+                  initialValue: data.privileges || [],
+                  valuePropName: 'targetKeys',
+                })(
+                  <Transfer
+                    dataSource={operationAuth} // 登陆用户有的所有权限
+                    showSearch
+                    render={item => item.title}
+                    searchPlaceholder={intl.formatMessage(commonMessages.inputPlaceholder)}
+                    notFoundContent={intl.formatMessage(commonMessages.dataNotFound)}
+                  />,
+                )
+              }
+            </FormItem>
             <FormItem
               {...formItemLayout}
               label={intl.formatMessage(commonMessages.remark)}
